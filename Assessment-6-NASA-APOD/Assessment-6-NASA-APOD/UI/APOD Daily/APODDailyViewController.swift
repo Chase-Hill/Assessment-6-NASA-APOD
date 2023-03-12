9051fa1b1b69256cba622475498b70cfb0affc37
Chase <chaseaohill@gmail.com>
2023-03-11 22:46:55 -0500

    Did some more quality of life visuals by adding a background image for each view. Wired up views to corresponding view controller.

--- Assessment-6-NASA-APOD/Assessment-6-NASA-APOD/UI/APOD Daily/APODDailyViewController.swift
+++ Assessment-6-NASA-APOD/Assessment-6-NASA-APOD/UI/APOD Daily/APODDailyViewController.swift
@@ -8,10 +8,31 @@
 import UIKit
 
 class APODDailyViewController: UIViewController {
-
+    
+    // MARK: - Outlets
+    @IBOutlet weak var apodTitleLabel: UILabel!
+    @IBOutlet weak var apodCopyrightLabel: UILabel!
+    @IBOutlet weak var todayAPODImageView: UIImageView!
+    @IBOutlet weak var apodExplanationLabel: UILabel!
+    
+    // MARK: - LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        assignbackground()
     }
     
-    
+    // MARK: - Functions
+    func assignbackground(){
+        let background = UIImage(named: "APOD background")
+        
+        var backGroundImageView : UIImageView!
+        backGroundImageView = UIImageView(frame: view.bounds)
+        backGroundImageView.contentMode =  UIView.ContentMode.scaleAspectFill
+        backGroundImageView.clipsToBounds = true
+        backGroundImageView.image = background
+        backGroundImageView.center = view.center
+        backGroundImageView.alpha = 0.75
+        view.addSubview(backGroundImageView)
+        self.view.sendSubviewToBack(backGroundImageView)
+    }
 }
--- Assessment-6-NASA-APOD/Assessment-6-NASA-APOD/UI/APOD Search/APODSearchViewController.swift
+++ Assessment-6-NASA-APOD/Assessment-6-NASA-APOD/UI/APOD Search/APODSearchViewController.swift
@@ -8,10 +8,32 @@
 import UIKit
 
 class APODSearchViewController: UIViewController {
-
+    
+    // MARK: - Outlets
+    @IBOutlet weak var apodDatePicker: UIDatePicker!
+    @IBOutlet weak var apodTitleLabel: UILabel!
+    @IBOutlet weak var apodCopyrightLabel: UILabel!
+    @IBOutlet weak var searchedAPODImageView: UIImageView!
+    @IBOutlet weak var apodExplanationLabel: UILabel!
+    
+    // MARK: - LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        assignbackground()
     }
     
-    
+    // MARK: - Functions
+    func assignbackground(){
+        let background = UIImage(named: "APOD search background")
+        
+        var backGroundImageView : UIImageView!
+        backGroundImageView = UIImageView(frame: view.bounds)
+        backGroundImageView.contentMode =  UIView.ContentMode.scaleAspectFill
+        backGroundImageView.clipsToBounds = true
+        backGroundImageView.image = background
+        backGroundImageView.center = view.center
+        backGroundImageView.alpha = 0.75
+        view.addSubview(backGroundImageView)
+        self.view.sendSubviewToBack(backGroundImageView)
+    }
 }
